3cdc85d3144ccc4d48e2ded060774791ccc99b96
JuliaB0920 <88746910+JuliaB0920@users.noreply.github.com>
2021-10-17 17:38:52 -0700

    Update ex_06_01.py

--- Course_2_Python_Data_Structures/ex_06_01.py
+++ Course_2_Python_Data_Structures/ex_06_01.py
@@ -1,3 +1,6 @@
+#Write a while loop that starts at the last character in the string and works its way backwards to the first character in the string, 
+#printing each letter on a separate line, except backwards.
+
 fruit = 'banana'
 length = -len(fruit)
 index = -1
